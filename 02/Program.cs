032749c6370f34b7b72dd47719b072779191f0c0
Joshua Geurts <jgeurts@ophardt.com>
2022-12-02 13:21:13 -0500

    Day two - used a sorted array to keep track of the 3 largest calories.

---
+++ 02/Program.cs
@@ -0,0 +1,28 @@
+﻿var size = 3;
+SortedLongArray arr = new SortedLongArray(size);
+using var sr = new StreamReader("../01/input.txt");
+long currentCalories = 0;
+while(!sr.EndOfStream){
+    var nextLine = await sr.ReadLineAsync();
+    if(nextLine == string.Empty){
+        arr.Add(currentCalories);
+        currentCalories = 0;
+    }
+    else{
+        if(int.TryParse(nextLine, out int calories)){
+            currentCalories += calories;
+        }
+        else{
+            throw new Exception("Invalid integer found in input! Input was: " + nextLine);
+        }
+    }
+}
+
+Console.WriteLine("Largest calories are: ");
+long totalOfAll = 0;
+for(int i=0; i<size; i++){
+    long calories = arr.GetAtIndex(i) ?? 0;
+    Console.WriteLine($"{i}: {calories}");
+    totalOfAll += calories;
+}
+Console.WriteLine("Total: " + totalOfAll);

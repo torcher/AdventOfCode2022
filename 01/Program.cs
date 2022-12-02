9402c73218fabbcb1148cbbff408f408244e5029
Joshua Geurts <jgeurts@ophardt.com>
2022-12-02 12:36:22 -0500

    Day one complete! My correct answer was 72240!

---
+++ 01/Program.cs
@@ -0,0 +1,21 @@
+﻿long largestCalories = 0;
+using var sr = new StreamReader("input.txt");
+long currentCalories = 0;
+while(!sr.EndOfStream){
+    var nextLine = await sr.ReadLineAsync();
+    if(nextLine == string.Empty){
+        if(currentCalories > largestCalories)
+            largestCalories = currentCalories;
+        currentCalories = 0;
+    }
+    else{
+        if(int.TryParse(nextLine, out int calories)){
+            currentCalories += calories;
+        }
+        else{
+            throw new Exception("Invalid integer found in input! Input was: " + nextLine);
+        }
+    }
+}
+
+Console.WriteLine("Largest calories was: " + largestCalories);

122c48e1c98721bd469d1370bb899e1d47f7b050
Joshua Geurts <jgeurts@ophardt.com>
2022-12-06 12:47:51 -0500

    Day 6 q2! Same problem as q1, but with a longer unique. made it generic, and specified the length in one line. More or less a one line change.

---
+++ 06/q2/Program.cs
@@ -0,0 +1,24 @@
+﻿char[]? inputChars = null;
+using var sr = new StreamReader("../input.txt");
+if(!sr.EndOfStream) inputChars = ((await sr.ReadLineAsync()) ?? "").ToCharArray();
+
+if(inputChars == null) throw new Exception("No input provided");
+
+int uniqueCharLength = 14;
+int pos = 0;
+for(int i=0; i<inputChars.Length-uniqueCharLength+1; i++){
+    var charsCount = new Dictionary<char, bool>();
+    for(int j=i; j<inputChars.Length && j < i+uniqueCharLength; j++){
+        if(charsCount.ContainsKey(inputChars[j]))
+            break;
+        else
+            charsCount[inputChars[j]] = true;
+    }
+    if(charsCount.Keys.Count == uniqueCharLength)
+    {
+        pos = i+uniqueCharLength;
+        break;
+    }
+}
+
+Console.WriteLine("The position of the last char of start-message-code is " + pos);

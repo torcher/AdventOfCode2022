90a6eff8344841ad15febf3ddc50faf4c1f2cbc5
Joshua Geurts <jgeurts@ophardt.com>
2022-12-06 12:47:01 -0500

    Day 6! Q1 wasn't too hard, fumbled the logic a little bit, but just went with a dictionary break-out that solved the problem!

---
+++ 06/q1/Program.cs
@@ -0,0 +1,23 @@
+﻿char[]? inputChars = null;
+using var sr = new StreamReader("../input.txt");
+if(!sr.EndOfStream) inputChars = ((await sr.ReadLineAsync()) ?? "").ToCharArray();
+
+if(inputChars == null) throw new Exception("No input provided");
+
+int pos = 0;
+for(int i=0; i<inputChars.Length-3; i++){
+    var charsCount = new Dictionary<char, bool>();
+    for(int j=i; j<inputChars.Length && j < i+4; j++){
+        if(charsCount.ContainsKey(inputChars[j]))
+            break;
+        else
+            charsCount[inputChars[j]] = true;
+    }
+    if(charsCount.Keys.Count == 4)
+    {
+        pos = i+4;
+        break;
+    }
+}
+
+Console.WriteLine("The position of the last char of start-code is " + pos);

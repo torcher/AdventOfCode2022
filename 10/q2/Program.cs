4d302f1d43f974ff9784a42492fbff57f682bd72
Torch <joshua.geurts@gmail.com>
2022-12-10 18:55:27 -0500

    Day 10 q2! Very fun, but not too complicated. ELPLZGZL!

---
+++ 10/q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+var x = 1;
+int cycle = 1;
+int? addToRegister = null;
+
+using var sr = new StreamReader("../input.txt");
+while(!sr.EndOfStream){
+  var pos = (cycle-1)%40;
+  if(x <= pos+1 && x >= pos-1) Console.Write("#");
+  else Console.Write(".");
+
+  if(addToRegister != null){
+    x += addToRegister ?? 0;
+    addToRegister = null;
+  }
+  else{
+    var line = (await sr.ReadLineAsync()) ?? "";
+    if(line != "noop"){
+      var lineSplit = line.Split(" ");
+      addToRegister = Convert.ToInt32(lineSplit[1]);
+    }
+  }
+
+  cycle++;
+  if((cycle-1)%40 == 0) Console.WriteLine();
+}

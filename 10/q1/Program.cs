f84bcff1218645bef42f95d0dd3a75eb9dff9633
Torch <joshua.geurts@gmail.com>
2022-12-10 16:30:29 -0500

    Day 10! Fun one, brought me back to assembly days a little bit. Not too hard, just a little hard to conceptualize.

---
+++ 10/q1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+var cyclesToCheck = new int[]{ 20,60,100,140,180,220 };
+var cycle = 0;
+var checkIndex = 0;
+var x = 1;
+var lastX = x;
+long signalSum = 0;
+
+using var sr = new StreamReader("../input.txt");
+while(!sr.EndOfStream){  
+  if(cycle >= cyclesToCheck[checkIndex]){
+    signalSum += cyclesToCheck[checkIndex]*lastX;
+    checkIndex++;
+    if(checkIndex == cyclesToCheck.Length)
+      break;
+  }
+
+  var line = (await sr.ReadLineAsync()) ?? "";
+  var lineSplit = line.Split(" ");
+  if(lineSplit[0] == "noop")
+  {
+    cycle++;
+    continue;
+  }
+
+  if(lineSplit[0] != "addx" | lineSplit.Length != 2)
+    throw new System.Exception("Invalid input: " + line);
+  
+  var v = Convert.ToInt32(lineSplit[1]);
+  lastX = x;
+  x += v;
+  cycle += 2;
+}
+
+Console.WriteLine("The sum of the signals is " + signalSum);
+
